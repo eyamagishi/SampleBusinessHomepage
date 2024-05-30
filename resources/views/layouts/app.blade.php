6f5ac7e554cfb1a42bfe84817a18652f140f781a
eyamagishi <e.yamagishi0625@gmail.com>
2024-05-30 20:47:39 +0900

    refactor: Fixed typo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
     <meta charset="UTF-8">
     <title>@yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="{{ __('messages.description_home_index') }}">
     @yield('meta')
     <link rel="stylesheet" href="css/style.css">
@@ -38,5 +36,4 @@
     <script src="js/main.js"></script>
     @yield('scripts')
 </body>
-
 </html>
