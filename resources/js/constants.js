099455a4d372c18d4bfda55cb1f0c297673cd16c
eyamagishi <e.yamagishi0625@gmail.com>
2024-06-06 23:03:26 +0900

    fix: 定数定義の変更

--- resources/js/constants.js
+++ resources/js/constants.js
@@ -1,8 +1,26 @@
 export const COMPANY_NAME = "Nicolas Bourbaki";
 
 export const MESSAGES = {
-    HOME:    "HOME",
-    SERVICE: "SERVICE",
-    MENU:    "MENU",
-    COMPANY: "COMPANY",
+    HOME:            "HOME",
+    SERVICE:         "SERVICE",
+    COMPANY:         "COMPANY",
+    CONTACT:         "CONTACT",
+    MENU:            "MENU",
+    INFORMATION:     "information",
+    WHAT_IS_NEW:     "What's New",
+    DESCRIPTION:     "LaravelとReactをviteで統合した勉強用サイトです。",
+    FOR_MORE_DETAIL: "詳しくみる",
+};
+
+export const TITLES = {
+    HOME: {
+        INDEX:   "ホームページトップ",
+        SERVICE: "サービス",
+    },
+};
+
+export const CATEGORIES = {
+    OTHER:     "その他",
+    IMPORTANT: "重要",
+    SERVICE:   "サービス",
 };
