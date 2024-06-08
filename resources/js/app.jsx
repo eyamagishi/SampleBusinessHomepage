9dfea39f94081d259d8f7fe2333cbd6a8466cb92
eyamagishi <e.yamagishi0625@gmail.com>
2024-06-08 18:33:05 +0900

    fix: inportの配置を修正

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,9 +5,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
-import Footer from './components/Footer';
 import Pagetop from './components/Pagetop';
 
 const App = () => {
